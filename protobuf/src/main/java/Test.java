7f23e41f0f7eaba4969847df0725d8f5ae89ee16
zhoushaofei <lxfeif123@126.com>
2019-01-23 14:41:04 +0800

    add spi project

--- protobuf/src/main/java/Test.java
+++ protobuf/src/main/java/Test.java
@@ -1,4 +1,5 @@
-import com.ekfet.protobuf.model.User;
+import com.ekfet.protobuf.model.PersonProtos;
+import com.google.protobuf.InvalidProtocolBufferException;
 
 /**
  * @author radish.zhou
@@ -7,7 +8,18 @@ import com.ekfet.protobuf.model.User;
  * @date 2019/1/23 10:39
  */
 public class Test {
-    public static void main(String[] args) {
-        User user= User.UserModel.newBuilder().
+    public static void main(String[] args) throws InvalidProtocolBufferException {
+        PersonProtos.Person.Builder builder= PersonProtos.Person.newBuilder();
+        builder.setEmail("lxfeif123@126.com");
+        builder.setId(100);
+        builder.setName("join");
+
+        PersonProtos.Person person=builder.build();
+
+        byte[] data=person.toByteArray();
+        PersonProtos.Person person1=PersonProtos.Person.parseFrom(data);
+        System.out.println(person.getEmail());
+        System.out.println(person1.getEmail());
+
     }
 }

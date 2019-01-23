86d1992080870c7bf38b203039ec4ecfbecd9740
zhoushaofei <lxfeif123@126.com>
2019-01-23 11:36:16 +0800

    add spi project

---
+++ protobuf/src/main/java/Test.java
@@ -0,0 +1,13 @@
+import com.ekfet.protobuf.model.User;
+
+/**
+ * @author radish.zhou
+ * @version v 1.0
+ * @ClassName: Test
+ * @date 2019/1/23 10:39
+ */
+public class Test {
+    public static void main(String[] args) {
+        User user= User.UserModel.newBuilder().
+    }
+}

058829ce78b50fe02131b30ae0c55a7c202094c7
radish.zhou <lxfeif123@126.com>
2019-09-24 17:49:40 +0800

    update

--- boot-mybatis-plus/src/main/java/com/ekfet/mybatis/plus/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.ekfet.mybatis.plus.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ekfet.mybatis.plus.model.User;
-
-public interface UserMapper extends BaseMapper<User> {
-}
